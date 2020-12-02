b0f0d68bc9d175da495d0e651d2fd3d9bcab79ad
shujingliew <50943484+shujingliew@users.noreply.github.com>
2020-12-02 16:35:32 +0800

    'BLEM'

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -4,11 +4,13 @@ import { invoiceTemplate } from "./invoice";
 import { letterCreditTemplate } from "./letterCredit";
 import { cugeCertTemplate } from "./cugeCert";
 import { templates as blTemplates } from "./billOfLading";
+import { templatesEM as blTemplatesEM } from "./billOfLadingEM";
 
 export const registry: TemplateRegistry<any> = {
   COVERING_LETTER: templates,
   LETTER_CREDIT: letterCreditTemplate,
   CUGE_CERT: cugeCertTemplate,
   BILL_OF_LADING: blTemplates,
+  BILL_OF_LADINGEM: blTemplatesEM,
   INVOICE: invoiceTemplate
 };
